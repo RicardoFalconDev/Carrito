e714efb042b3183c487e96552a3e96e17079fedc
Ricardo Falcon <ricardofalcondev@gmail.com>
2022-12-06 23:38:19 -0300

    creacion archivo

---
+++ index.js
@@ -0,0 +1,14 @@
+import { actualizarCarrito } from "./actualizarCarrito.js";
+import { mostrarProductos } from "./App.js";
+import { obtenerCarritoStorage, renderProductosCarrito } from "./carritoIndex.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  mostrarProductos();
+
+  if (localStorage.getItem("carrito")) {
+    const carritoStorage = obtenerCarritoStorage();
+    renderProductosCarrito(carritoStorage);
+    actualizarCarrito(carritoStorage);
+  }
+})
